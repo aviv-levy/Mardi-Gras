a7bad1696cacc1f4517f6fef032fa6f6814497bf
Aviv Levy <avvvviv@walla.co.il>
2022-08-22 21:49:31 +0300

    Some changes

---
+++ JS/contact.js
@@ -0,0 +1,16 @@
+let enterDetails = ()=>{
+    let name = document.getElementById('name').value;
+    let lname = document.getElementById('lname').value;
+    let email = document.getElementById('email').value;
+    let textarea = document.getElementById('textarea').value;
+
+    
+}
+
+function validation(name,lname,email,textarea){
+
+}
+
+function emailValidate(){
+
+}
